c8d2729e66355b577f9b6865356dc2d2fe147cf3
Nazarii Khalimonov <nazar131188@gmail.com>
2025-03-26 00:00:26 +0200

    created workers list page and tasktypes list page

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from manager.views import index, PositionListView
+from manager.views import (
+    index,
+    PositionListView,
+    TaskTypeListView,
+    WorkerListView,
+)
 
 urlpatterns = [
     path("home/", index, name="home"),
     path("position/", PositionListView.as_view(), name="position-list"),
+
+    path("tasktype/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("worker/", WorkerListView.as_view(), name="worker-list"),
 ]
 
 app_name = "manager"
--- manager/views.py
+++ manager/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from manager.models import Worker, Position
+from manager.models import Worker, Position, TaskType
 from django.views.generic import ListView
 
 
@@ -16,3 +16,15 @@ class PositionListView(ListView):
     model = Position
     template_name = "manager/position_list.html"
     context_object_name = "position_list"
+
+
+class TaskTypeListView(ListView):
+    model = TaskType
+    template_name = "manager/task_type_list.html"
+    context_object_name = "task_type_list"
+
+
+class WorkerListView(ListView):
+    model = Worker
+    template_name = "manager/worker_list.html"
+    context_object_name = "worker_list"
