cd564c7f5a404cc900634bee7ce16fbf92eee9de
Nazarii Khalimonov <nazar131188@gmail.com>
2025-03-25 23:42:21 +0200

    created positions list page with all position names

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from manager.views import index
+from manager.views import index, PositionListView
 
 urlpatterns = [
     path("home/", index, name="home"),
-
+    path("position/", PositionListView.as_view(), name="position-list"),
 ]
 
 app_name = "manager"
--- manager/views.py
+++ manager/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from manager.models import Worker
+from manager.models import Worker, Position
+from django.views.generic import ListView
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -10,3 +11,8 @@ def index(request: HttpRequest) -> HttpResponse:
         "workers": workers,
     }
     return render(request, "manager/index.html", context=context)
+
+class PositionListView(ListView):
+    model = Position
+    template_name = "manager/position_list.html"
+    context_object_name = "position_list"
